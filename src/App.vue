aa7cda941c8626b31aa74fd21cbf8904e5b41059
Qsort <awpfriend@sina.com>
2019-08-23 16:35:24 +0800

    Add Classification.vue

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,26 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <Classification></Classification>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import Classification from "./components/Classification/Classification";
+
+    export default {
+        name: 'App',
+        components: {Classification}
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
