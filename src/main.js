343a8108945bf2d1efdae018504f9c01902f9423
Qsort <awpfriend@sina.com>
2019-08-25 09:51:14 +0800

    import axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
