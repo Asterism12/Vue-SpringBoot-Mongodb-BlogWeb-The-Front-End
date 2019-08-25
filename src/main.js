600cc6596f1dd57a204cd539e5e636d41d6d75bd
Qsort <awpfriend@sina.com>
2019-08-25 10:01:12 +0800

    import axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
-var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
 
 Vue.prototype.$axios = axios
