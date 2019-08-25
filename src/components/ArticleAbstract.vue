3e666054974775fa5904cc5aab13515cae0e225a
Qsort <awpfriend@sina.com>
2019-08-25 11:19:47 +0800

    add article list & article abstract

---
+++ src/components/ArticleAbstract.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ArticleAbstract"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ArticleList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ArticleList"
+    }
+</script>
+
+<style scoped>
+
+</style>
