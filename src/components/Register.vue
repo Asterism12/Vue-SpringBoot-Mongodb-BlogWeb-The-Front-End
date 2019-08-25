303c7d14ecefa1cf0cee692fb4f2213616d839a4
Qsort <awpfriend@sina.com>
2019-08-25 14:57:32 +0800

    add register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="log">
+    用户名:<input type="text" v-model="registerForm.username" placeholder="请输入用户名"/>
+    <br><br>
+    密码： <input type="password" v-model="registerForm.password" placeholder="请输入密码"/>
+    <br><br>
+    <button v-on:click="login">注册</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Register',
+        data () {
+            return {
+                registerForm: {
+                    username: '',
+                    password: ''
+                },
+                responseResult: []
+            }
+        },
+        methods: {
+            login () {
+                this.$axios
+                    .post('api/login', {
+                        username: this.loginForm.username,
+                        password: this.loginForm.password
+                    })
+                    .then(successResponse => {
+                        alert("success jieshou")
+                        if (successResponse.data.code === 200) {
+                            this.$router.replace({path: '/HelloWorld'})
+                        }else{
+                            this.$router.replace({path: '/HelloWorld'})
+                        }
+                    })
+                    .catch(failResponse => {
+                    })
+            }
+        }
+    }
+</script>
+
+<style>
+  .log {
+    margin-top: 200px;
+  }
+</style>
