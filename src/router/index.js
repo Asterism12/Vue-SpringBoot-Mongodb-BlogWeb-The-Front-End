33e25a62ddff142a6952884d7e1a07a2d7332ba0
Qsort <awpfriend@sina.com>
2019-08-25 11:36:45 +0800

    add Blog

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from "../components/Home";
+import Home from "@/components/Home"
 import Login from '@/components/Login'
+import Blog from "@/components/Blog";
 
 Vue.use(Router)
 
@@ -17,5 +18,10 @@ export default new Router({
       name: 'Login',
       component: Login
     },
+    {
+      path: '/blog/:id',
+      name: 'Blog',
+      components: Blog
+    }
   ]
 })
