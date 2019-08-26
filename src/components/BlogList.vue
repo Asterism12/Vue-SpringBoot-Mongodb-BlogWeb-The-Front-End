574c8f6bdf111fcf11235dfa63ed15dffd8602b5
Qsort <awpfriend@sina.com>
2019-08-26 10:37:54 +0800

    add blog list

---
+++ src/components/BlogList.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-row>
+    <el-table :data="blog.comments">
+      <el-table-column prop="username" label="用户名">
+      </el-table-column>
+
+      <el-table-column prop="content" label="评论">
+      </el-table-column>
+    </el-table>
+  </el-row>
+</template>
+
+<script>
+    export default {
+        name: "BlogList",
+        data(){
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
