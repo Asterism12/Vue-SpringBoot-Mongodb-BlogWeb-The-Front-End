f2c4b416df9986e7eef74fa8ad1ff35e8bdfc59f
Qsort <awpfriend@sina.com>
2019-08-26 11:28:18 +0800

    test

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -2,7 +2,7 @@
   <el-card class="box-card" :data="blog">
     <div slot="header" class="clearfix">
       <span>{{blog.title}}</span>
-      <el-button style="float: right; padding: 3px 0" type="text" @click="GoToRoute"()>查看原文</el-button>
+      <el-button style="float: right; padding: 3px 0" type="text" @click="GoToBlog"(blog.bid)>查看原文</el-button>
     </div>
     <div  class="text item">
       {{blog.abstract}}
@@ -19,7 +19,7 @@
             }
         },
         methods:{
-            GoToRoute : function(){
+            GoToBlog : function(bid){
                 this.$router.push({ name: 'blog', params: { id: blog.bid }})
             }
         }
