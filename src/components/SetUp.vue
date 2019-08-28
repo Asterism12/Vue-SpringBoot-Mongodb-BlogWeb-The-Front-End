03a731d276289829b7ee6bc56bf7ab7da2d2230c
Joe <807831257@qq.com>
2019-08-28 16:50:54 +0800

    comment

---
+++ src/components/SetUp.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-row>
+      <el-col>
+        <el-card>
+          头像设置
+        </el-card>
+      </el-col>
+      <el-col>
+        <el-avatar></el-avatar>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        密码设置
+      </el-col>
+      <el-col>
+        <el-input placeholder="请输入新密码" show-password>
+        </el-input>
+      </el-col>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+    export default {
+        name: "SetUp"
+    }
+</script>
+
+<style scoped>
+
+</style>
