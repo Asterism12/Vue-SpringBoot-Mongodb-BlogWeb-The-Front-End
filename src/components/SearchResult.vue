f6ef6c05cf6842cda882ef88713004f334cdeefd
Qsort <awpfriend@sina.com>
2019-08-28 09:27:34 +0800

    bug fix

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,68 +1,70 @@
 <template>
-  <el-row>
-    <el-col :span="18">
-      <div class="grid-content bg-purple-dark">
-        <el-input v-model="input" placeholder="请输入内容"></el-input>
-      </div>
-    </el-col>
-    <el-col :span="4"></el-col>
-    <el-col :span="2">
-      <div class="grid-content bg-purple-dark">
-        <el-button @click="Search()">开始搜索</el-button>
-      </div>
-    </el-col>
-  </el-row>
-  <el-row>
-    <el-col :span="12">
-      <div class="grid-content bg-purple">
-        <p>搜索文章</p>
-        <el-card class="box-card" v-for="blogresult in blogresults":key="bid">
-          <div slot="header" class="clearfix">
-            <span>{{blogresult.title}}</span>
-            <el-button style="float: right; padding: 3px 0" type="text"></el-button>
-          </div>
-          <div class="text item">
-            {{blogresult.abstract}}
-          </div>
-        </el-card>
-      </div>
-    </el-col>
-    <el-col :span="12">
-      <div class="grid-content bg-purple-light">
-        <p>搜索用户</p>
-        <el-card class="box-card" v-for="userresult in userresults":key="uid">
-          <div slot="header" class="clearfix">
-            <span>{{userresult.username}}</span>
-            <el-button style="float: right; padding: 3px 0" type="text"></el-button>
-          </div>
-          <div class="text item">
-            {{userresult.userinfo}}
-          </div>
-        </el-card>
-      </div>
-    </el-col>
-  </el-row>
+  <div>
+    <el-row>
+      <el-col :span="18">
+        <div class="grid-content bg-purple-dark">
+          <el-input v-model="input" placeholder="请输入内容"></el-input>
+        </div>
+      </el-col>
+      <el-col :span="4"></el-col>
+      <el-col :span="2">
+        <div class="grid-content bg-purple-dark">
+          <el-button @click="Search()">开始搜索</el-button>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          <p>搜索文章</p>
+          <el-card class="box-card" v-for="blogresult in blogresults" :key="bid">
+            <div slot="header" class="clearfix">
+              <span>{{blogresult.title}}</span>
+              <el-button style="float: right; padding: 3px 0" type="text"></el-button>
+            </div>
+            <div class="text item">
+              {{blogresult.abstract}}
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light">
+          <p>搜索用户</p>
+          <el-card class="box-card" v-for="userresult in userresults" :key="uid">
+            <div slot="header" class="clearfix">
+              <span>{{userresult.username}}</span>
+              <el-button style="float: right; padding: 3px 0" type="text"></el-button>
+            </div>
+            <div class="text item">
+              {{userresult.userinfo}}
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
     export default {
         name: "SearchResult",
-        data(){
-            return{
+        data() {
+            return {
                 input: '',
-                blogresults:[{title:"title",abstract:"abstract",bid:"1"}],
-                userresults:[{username:"Asterism",userinfo:"happy"}]
+                blogresults: [{title: "title", abstract: "abstract", bid: "1"}],
+                userresults: [{username: "Asterism", userinfo: "happy"}]
             }
         },
         created() {
-            input=this.$route.params.keyword
+            input = this.$route.params.keyword
             this.Search()
         },
-        methods:{
-            Search:function () {
+        methods: {
+            Search: function () {
                 this.$axios.get('/lists', {
                     params: {
-                        keyword:this.input,
+                        keyword: this.input,
                     }
                 })
                     .then(response => {
@@ -72,7 +74,7 @@
                     })
                 this.$axios.get('/userlists', {
                     params: {
-                        keyword:this.input,
+                        keyword: this.input,
                     }
                 })
                     .then(response => {
@@ -99,6 +101,7 @@
     display: table;
     content: "";
   }
+
   .clearfix:after {
     clear: both
   }
