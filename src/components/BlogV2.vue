744406d1e0c14009343c57150261278ae2537c33
Qsort <awpfriend@sina.com>
2019-08-29 11:05:41 +0800

    fix bug

--- src/components/BlogV2.vue
+++ src/components/BlogV2.vue
@@ -6,20 +6,20 @@
     <mavon-editor
       class="md"
       :value="this.blog.content"
-      :subfield = "prop.subfield"
-      :defaultOpen = "prop.defaultOpen"
-      :toolbarsFlag = "prop.toolbarsFlag"
+      :subfield="prop.subfield"
+      :defaultOpen="prop.defaultOpen"
+      :toolbarsFlag="prop.toolbarsFlag"
       :editable="prop.editable"
       :scrollStyle="prop.scrollStyle"
       style="margin-top: 20px"
     ></mavon-editor>
     <el-row>
       <div>
-        <el-card class="box-card" v-for="comment in blog.comments":key="blog.comments.bid">
+        <el-card class="box-card" v-for="comment in blog.comments" :key="blog.comments.bid">
           <div slot="header" class="clearfix">
             <span>{{comment.username}}</span>
           </div>
-          <div  class="text item">
+          <div class="text item">
             {{comment.content}}
           </div>
         </el-card>
@@ -33,22 +33,23 @@
 
 <script>
     import CommentEditor from "./CommentEditor";
+
     export default {
         name: "BlogV2",
         components: {CommentEditor},
-        data () {
+        data() {
             return {
-                blog: {title:'',content:'', comments:[]}
+                blog: {title: '', content: '', comments: []}
             }
-    },
+        },
         computed: {
-            prop () {
+            prop() {
                 let data = {
-                        subfield: false,// 单双栏模式
-                        defaultOpen: 'preview',//edit： 默认展示编辑区域 ， preview： 默认展示预览区域
-                        editable: false,
-                        toolbarsFlag: false,
-                        scrollStyle: true
+                    subfield: false,// 单双栏模式
+                    defaultOpen: 'preview',//edit： 默认展示编辑区域 ， preview： 默认展示预览区域
+                    editable: false,
+                    toolbarsFlag: false,
+                    scrollStyle: true
                 }
                 return data
             }
